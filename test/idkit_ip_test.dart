44fb64b4f69f3a38539af33a1d35f6ba047f8fc4
shuangjian.zhou <1542100658@qq.com>
2021-12-23 16:37:24 +0800

    去除代码警告

--- test/idkit_ip_test.dart
+++ test/idkit_ip_test.dart
@@ -3,16 +3,17 @@ library idkit_ip;
 import 'package:idkit_ip/idkit_ip.dart';
 import 'package:idkit_ip/src/ip_config.dart';
 
+// ignore: avoid_void_async
 void main() async {
-  String ip1 = await IDKitIp.getIPv4(format: IPFormat.json);
+  final String ip1 = await IDKitIp.getIPv4(format: IPFormat.json);
   print(ip1);
 
-  String ip = await IDKitIp.getIPv4();
+  final String ip = await IDKitIp.getIPv4();
   print(ip);
 
-  String ip2 = await IDKitIp.getIPv6(format: IPFormat.json);
+  final String ip2 = await IDKitIp.getIPv6(format: IPFormat.json);
   print(ip2);
 
-  String ip3 = await IDKitIp.getIPv6();
+  final String ip3 = await IDKitIp.getIPv6();
   print(ip3);
 }
